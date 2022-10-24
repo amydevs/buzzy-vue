05445a1d07422b546bebafcd88df4eb97f9a8263
amy <ctrlalternativedelete@gmail.com>
2022-10-24 15:12:40 +1100

    updated to latest buttplug version

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack')
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: false,
   configureWebpack: {
     experiments: {
       syncWebAssembly: true
